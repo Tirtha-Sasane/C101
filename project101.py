aa5869bef91a3651edf3d3565b09d5238730bb62
Tirtha-Sasane <75529276+Tirtha-Sasane@users.noreply.github.com>
2023-07-29 15:51:51 +0530

    Add files via upload

---
+++ project101.py
@@ -0,0 +1,44 @@
+import random
+
+dice = 'y'
+
+while(dice=='y'):
+   
+    no = random.randint(1,6)
+    if(no==1):
+        print("[-----]")
+        print('[     ]')
+        print('[  0  ]')
+        print('[     ]')
+        print("[-----]")
+    elif(no==2):
+        print("[-----]")
+        print('[     ]')
+        print('[0   0]')
+        print('[     ]')
+        print("[-----]")
+    elif(no==3):
+        print("[-----]")
+        print('[     ]')
+        print('[0 0 0]')
+        print('[     ]')
+        print("[-----]")
+    elif(no==4):
+        print("[-----]")
+        print('[0   0]')
+        print('[     ]')
+        print('[0   0]')
+        print("[-----]")
+    elif(no==5):
+        print("[-----]")
+        print('[0   0]')
+        print('[  0  ]')
+        print('[0   0]')
+        print("[-----]")
+    elif(no==6):
+        print("[-----]")
+        print('[0   0]')
+        print('[0   0]')
+        print('[0   0]')
+        print("[-----]")
+    dice = input('do you want to roll the dice ? (y)')
